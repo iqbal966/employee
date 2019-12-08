7922ce9656312063b993d017a2bcc6a6bd014603
iqbal966 <48712723+iqbal966@users.noreply.github.com>
2019-12-08 15:34:06 +0800

    admin

--- resources/views/admin.blade.php
+++ resources/views/admin.blade.php
@@ -2,13 +2,13 @@
 
 @section('content')
 
-form id="apply-leave" method="post" action="">
+<form id="staff-name" method="post" action="">
         <div class="card-body">
             <div class="row">
                 <div class="col-12">
                     <div class="form-group pmd-textfield pmd-textfield-floating-label ">
-                        <label for="leave-type">stad name</label>
-                        <select name="leave-type" id="leave-type" class="form-control" title="Please select a Leave Type" required>
+                        <label for="leave-type">staff-name</label>
+  <select name="leave-type" id="leave-type" class="form-control" title="Please select a Staff Name" required>
                             <option></option>
                             <option>ema</option>
                             <option>wani</option>
@@ -18,6 +18,40 @@ form id="apply-leave" method="post" action="">
                         </select>
                     </div>
                 </div>
+        <form id="shift" method="post" action="">
+                    <div class="card-body">
+                        <div class="row">
+                            <div class="col-12">
+                 <div class="form-group pmd-textfield pmd-textfield-floating-label ">
+                                    <label for="leave-type">shift</label>
+ <select name="leave-type" id="leave-type" class="form-control" title="Please select a Shift Type" required>
+                                        <option></option>
+                                        <option>Morning</option>
+                                        <option>Middle</option>
+                                        <option>Night</option>
+                                        
+                                    </select>
+                                </div>
+                            </div>
+         <form id="shift" method="post" action="">
+                    <div class="card-body">
+                                    <div class="row">
+                                        <div class="col-12">
+                 <div class="form-group pmd-textfield pmd-textfield-floating-label ">
+                                     <label for="leave-type">shift</label>
+         <select name="leave-type" id="leave-type" class="form-control" title="Please select a Staff ID" required>
+                                                    <option></option>
+                                <option>Morning</option>
+                                <option>Middle</option>
+                                <option>Night</option>
+                                                    
+                                                </select>
+                                            </div>
+                                        </div>
+                            
+        
+
+
 
 
                 @endsection
